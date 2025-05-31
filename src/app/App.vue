7d733f184550f8ddfecc3aaaf739056b83f95f51
aidosgal <agalimzhan928@gmail.com>
2025-05-31 14:02:09 +0500

    feat(app): add fsd architecture

---
+++ src/app/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div>
+        <a href="https://vite.dev" target="_blank">
+            <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+    </div>
+</template>
